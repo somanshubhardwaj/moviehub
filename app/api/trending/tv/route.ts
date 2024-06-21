667724cf7a84a87da0cd940b95a45fdb9ac152b2
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-06-21 11:45:40 +0530

    fixes

--- app/api/trending/tv/route.ts
+++ app/api/trending/tv/route.ts
@@ -1,6 +1,6 @@
-import type { NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
-export async function GET( res: NextApiResponse) {
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
   const token: any =process.env.MOVIEDBAPIKEY;
   try {
     const response = await fetch(
