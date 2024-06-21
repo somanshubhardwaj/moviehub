f2908ebd06c7366ea5c74d722388d9718bac2a94
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-06-21 11:27:13 +0530

    created backend API's

---
+++ app/api/movielist/popular/route.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+  const token: string = "5532a84479c2fcd0e6f9cfd297028a8b";
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&region=in&api_key=${token}`
+    );
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (e: any) {
+    res.status(500).json({ message: e.message });
+  }
+}
